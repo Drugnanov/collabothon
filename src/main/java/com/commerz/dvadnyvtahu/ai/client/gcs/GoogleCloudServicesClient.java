0d41983f2883be77ff7dfaafd7e3b33873f795a7
Ondrej Hornicek <andrehornicek@gmail.com>
2023-10-21 13:23:14 +0200

    Fix

--- src/main/java/com/commerz/dvadnyvtahu/ai/client/gcs/GoogleCloudServicesClient.java
+++ src/main/java/com/commerz/dvadnyvtahu/ai/client/gcs/GoogleCloudServicesClient.java
@@ -8,8 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-
 
 @FeignClient(name = "GoogleCloudServicesClient", url = "https://storage.googleapis.com/",
         configuration = FeignSupportConfig.class)
@@ -17,7 +15,7 @@ import java.io.File;
 public interface GoogleCloudServicesClient {
     @PostMapping(value= "/upload/storage/v1/b/collabothon-dvadnyvtahu/o?uploadType=media&name={fileName}", consumes = MediaType.IMAGE_JPEG_VALUE)
     @Headers("Content-Type: image/jpeg")
-    String uploadImage(@RequestHeader("Authorization") String token, @PathVariable String fileName, @RequestBody File file);
+    String uploadImage(@RequestHeader("Authorization") String token, @PathVariable String fileName, @RequestPart(value = "file") MultipartFile file);
 
     @PutMapping(value = "/storage/v1/b/collabothon-dvadnyvtahu/iam", consumes = MediaType.APPLICATION_JSON_VALUE)
     String setPublicAccess(@RequestHeader("Authorization") String token, @RequestBody String body);
